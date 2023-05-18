794ebf3c9ca5055415113a88b6ca88e767a2dabf
yuekaixin <kaixin.yue@salesdriver.com.cn>
2023-05-18 19:38:52 +0800

    feat: 新增less和elementPlus

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,7 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
-import eslintPlugin from 'vite-plugin-eslint' //导入包
+import { defineConfig } from 'vite';
+import vue from '@vitejs/plugin-vue';
+import eslintPlugin from 'vite-plugin-eslint';
+import path from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -8,4 +9,22 @@ export default defineConfig({
         eslintPlugin({
             include: ['src/**/*.js', 'src/**/*.vue', 'src/*.js', 'src/*.vue']
         })],
-})
+    resolve: {
+        'map': [
+            ['@src', path.resolve(__dirname, './src')]
+        ]
+    },
+    // 引入less
+    css: {
+        preprocessorOptions: {
+            less: {
+                javascriptEnabled: true,
+                additionalData: `@import "${path.resolve(
+                    __dirname,
+                    'src/assets/index.less',
+                )}";`,
+            },
+        },
+    },
+
+});
